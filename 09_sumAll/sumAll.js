317d90563dfa7b333164fe9e131622a34a7504c4
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2025-12-13 12:31:47 +0700

    Finish 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
 
+    if (a <= 0 || b <= 0) {
+        return 'ERROR';
+    }
+
+    let bigger = a > b ? a : b;
+    let smaller = a > b ? b : a;
+    let sum = 0;
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
