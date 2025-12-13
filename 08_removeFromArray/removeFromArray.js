d6297fb7d4c70dd5ba9415b91d6175288658e092
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2025-12-13 12:19:13 +0700

    Finish 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...others) {
+    let removeFromArray = others;
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (removeFromArray.includes(array[i])) {
+            array.splice(i, 1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
