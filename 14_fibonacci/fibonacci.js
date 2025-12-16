c14d2b32267787cea9ed067ba5486eeb257cbb92
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2025-12-16 16:24:53 +0700

    Finish 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = Number(a)
+    if (a == 0) {
+        return 0
+    }
+    if (a < 0) {
+        return 'OOPS'
+    }
+    if (a == 1 || a == 2) {
+        return 1
+    }
+    return fibonacci(a-1) + fibonacci(a-2)
 };
 
 // Do not edit below this line
