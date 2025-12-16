f01bcec783cddb7a9955ec5bc2b9875adf8e51b9
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2025-12-16 16:34:01 +0700

    Finish 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((obj, item) => {
+        let objAge, itemAge
+        if ('yearOfDeath' in obj) {
+            objAge = obj.yearOfDeath - obj.yearOfBirth
+        } else {
+            objAge = 2025 - obj.yearOfBirth
+        }
+        if ('yearOfDeath' in item) {
+            itemAge = item.yearOfDeath - item.yearOfBirth
+        } else {
+            itemAge = 2025 - obj.yearOfBirth
+        }
+        if (itemAge > objAge) {
+            return item
+        }
+        return obj
+    }, people[0])
 };
 
 // Do not edit below this line
