f6d2fba79fe06a1262e7d5bb27d219a271701731
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2025-12-16 16:20:54 +0700

    Finish 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+function isAlphaNumeric(str) {
+    return /^[a-zA-Z0-9]+$/.test(str);
+}
 
+const palindromes = function (str) {
+    let start = 0
+    let end = str.length - 1
+
+    while (start < end) {
+        if (!isAlphaNumeric(str.charAt(start))){
+            start++
+            continue
+        }
+        if (!isAlphaNumeric(str.charAt(end))){
+            end--
+            continue
+        }
+        if (str.charAt(start).toLowerCase() !== str.charAt(end).toLowerCase()) {
+            return false
+        }
+        start++
+        end--
+    }
+    return true;
 };
 
 // Do not edit below this line
